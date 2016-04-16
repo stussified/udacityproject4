d0bd62bfc9253b2ba10c2c7c3a06094f42e8a6b0
Anthony Wong <anthonyw@sitescout.com>
2016-04-15 20:52:46 -0400

    most of it done, scoring doesn't work

---
+++ main.py
@@ -0,0 +1,37 @@
+# Contains taskqueue and cron jobs.  Heavily inspired by main.py from Skeleton Project Guess a Number
+
+import logging
+
+import webapp2
+from google.appengine.api import mail, app_identity
+from api import BetweenTheSheets
+from models import User
+
+class SendReminderEmail(webapp2.RequestHandler):
+	# send an email reminder to users about games using cron
+	def get(self):
+		app_id = app_identity.get_application_id()
+		users = User.query(User.email != None)
+		for user in users:
+			subject = 'Keep the streak alive!'
+			body = "Hello {}, keep on playing Between the sheets!".format(user.name)
+			# This will send test emails
+			mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+				user.email,
+				subject,
+				body)
+
+
+
+class UpdateMaxStreak(webapp2.RequestHandler):
+	def post(self):
+		# Update longest streak in memcache
+		BetweenTheSheets._cache_longest_streak()
+		self.response.set_status(204)
+
+# I have a feeling the thing to update the random numbers is going to be in here.
+
+app = webapp2.WSGIApplication([
+	('/crons/send_reminder', SendReminderEmail),
+	('tasks/get_longest_streak', UpdateMaxStreak),
+	], debug=True)
---
+++ utils.py
@@ -0,0 +1,28 @@
+# Utils.py heavily inspired by utils.py seen in guess a number project.
+
+import logging
+from google.appengine.ext import ndb
+import endpoints
+
+def get_by_urlsafe(urlsafe, model):
+	""" Retrieves ndb.Model entity that the urlsafe value points to.
+	If it doens't work, it returns an exception.
+	"""
+
+	try:
+		key = ndb.Key(urlsafe=urlsafe)
+	except TypeError:
+		raise endpoints.BadRequestException('Invalid Key')
+	except Exception, e:
+		if e.__class__.__name__ == 'ProtocolBufferDecodeError':
+			raise endpoints.BadRequestException('Invalid Key')
+		else:
+			raise
+
+	entity = key.get()
+	if not entity:
+		return None
+	if not isinstance(entity, model):
+		raise ValueError('Incorrect Kind')
+	return entity
+	
